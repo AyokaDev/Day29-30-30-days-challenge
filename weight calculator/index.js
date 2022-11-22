2d1070177d8309c06e874c3e77358cf31184df5e
AyokaDev <102034372+AyokaDev@users.noreply.github.com>
2022-11-22 03:33:56 -0800

    Delete index.js

--- weight calculator/index.js
+++
@@ -1,6 +0,0 @@
-function change() {
-    let input = document.getElementById("input").value;
-    let sap = document.getElementById("sap");
-    let kgtoPound = input * 2.20462;
-    sap.innerHTML = kgtoPound
-}
